29bfd4b2a910c629f403be2e683b3dee0a24cb8b
Alana Alexander <laninsky@gmail.com>
2018-05-24 11:12:55 +1200

    Update filtering_SNPs_by_sample_coverage.R

--- filtering_SNPs_by_sample_coverage.R
+++ filtering_SNPs_by_sample_coverage.R
@@ -1,7 +1,9 @@
 input_file <- commandArgs()[1]
 
-  cat("Call this function by filtering_SNPs_by_sample_coverage(working_dir,input_file)")
-  cat('e.g. filtering_SNPs_by_sample_coverage("./","results_0.1_type0a_TEST.fa")')
+print(input_file)
+  cat("Rscript filtering_SNPs_by_sample_coverage.R path_and_name_of_file_you_want_to_filter
+e.g.
+Rscript filtering_SNPs_by_sample_coverage.R results_21merged_trimmedQ20_R1_21merged_trimmedQ20_R2_22merged_trimmedQ20_R1_22merged_trimmedQ20_R2_23merged_trimmedQ20_R1_23merged_trimmedQ20_R2_24merged_trimmedQ20_R1_24merged_trimmedQ20_R2_25merged_trimmedk51_coherents_type_0a.fa")
   input <- read.table(input_file,header = FALSE,sep = "\t")
   to_delete <- NULL
 
