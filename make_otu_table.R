27d48cba3842ebac75a87f7c8dfc0884f231e68c
Alana Alexander <laninsky@gmail.com>
2018-09-13 12:16:54 +1200

    Update make_otu_table.R

--- make_otu_table.R
+++ make_otu_table.R
@@ -1,7 +1,3 @@
-fasta_file <- "/Users/alanaalexander/Downloads/18S_1_sorted_OTU_table.fasta"
-
-
-
 make_otu_table <- function(fasta_file) {
   temp <- readLines(fasta_file)
   temp <- temp[grep(">",temp)]
@@ -10,6 +6,24 @@ make_otu_table <- function(fasta_file) {
     temp1 <- unlist(strsplit(temp[i],"merged_sample=\\{"))[2]
     temp2 <- unlist(strsplit(temp1,"\\}; cluster_weight=[[:digit:]]+; cluster="))
     otuname <- gsub("; cluster_score=.*","",temp2[2])
-  
-  
+    temp2 <- unlist(strsplit(temp2[1],":"))
+    temp2 <- unlist(strsplit(temp2,","))
+    temp2 <- gsub(" ","",temp2)
+    temp2 <- gsub("'","",temp2)
+    if (is.null(outputmatrix)) {
+      outputmatrix <- matrix("",ncol=(length(temp2)/2+1),nrow=2)
+      outputmatrix[1,] <- c("",temp2[seq(1,length(temp2),2)])
+      outputmatrix[2,] <- c(otuname,temp2[seq(2,length(temp2),2)])
+    } else {
+      if (!(otuname %in% outputmatrix[,1])) {
+        outputmatrix <- rbind(outputmatrix,c(otuname,rep.int(0,(dim(outputmatrix)[2]-1))))
+      }  
+      for (j in seq(1,length(temp2),2)) {
+        if (!(temp2[j] %in% outputmatrix[1,])) {
+          outputmatrix <- cbind(outputmatrix,c(temp2[j],rep.int(0,(dim(outputmatrix)[1]-1))))
+        }
+        outputmatrix[(which(outputmatrix[,1]==otuname)),(which(outputmatrix[1,]==temp2[j]))] <- as.numeric(outputmatrix[(which(outputmatrix[,1]==otuname)),(which(outputmatrix[1,]==temp2[j]))]) + as.numeric(temp2[j+1])
+      }
+    }  
+  }
 }
